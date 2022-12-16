3009b523876c98ef28f7859ef1e77bc6810d0bbf
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-12-16 13:52:06 +0900

    Setting

---
+++ src/main/java/com/ll/re_batch/ReBatchApplication.java
@@ -0,0 +1,13 @@
+package com.ll.re_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/re_batch/ReBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.re_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
