d8e6701b5e884746cd3d0d8584a61b3bdf4559f7
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-12-16 13:53:20 +0900

    Feat : Spring Boot App -> Batch App 변경

--- src/main/java/com/ll/re_batch/ReBatchApplication.java
+++ src/main/java/com/ll/re_batch/ReBatchApplication.java
@@ -1,9 +1,11 @@
 package com.ll.re_batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing // Spring Boot App -> Batch App => 스키마 생성
 public class ReBatchApplication {
 
     public static void main(String[] args) {
