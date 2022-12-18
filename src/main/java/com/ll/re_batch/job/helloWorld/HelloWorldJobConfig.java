45ca28bd6da7959746a0ec6232e6e05f6e590717
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-12-19 00:59:59 +0900

    Feat : 설명 추가, increment로 반복 실행

--- src/main/java/com/ll/re_batch/job/helloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/ll/re_batch/job/helloWorld/HelloWorldJobConfig.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -19,15 +22,19 @@ public class HelloWorldJobConfig {
     private final StepBuilderFactory stepBuilderFactory;
 
     // 하나의 Job 생성 -> 1job에는 다수의 step
-    @Bean
+    @Bean // 공유자원 -> 해당 객체를 어디서든지 사용할 수 있음
+    @JobScope // Bean, Session 등 각 영역마다 객체 생성
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
+                // Job이 성공하면 다시 실행되지 않는다. 하지만, increment로 다시 실행시킬 수 있다.
+                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
                 .start(helloWorldStep1())
                 .build();
     }
 
     // Job에 들어갈 step 생성
     @Bean
+    @StepScope
     public Step helloWorldStep1() {
         return stepBuilderFactory.get("helloWorldStep1")
                 .tasklet(helloWorldTasklet())
