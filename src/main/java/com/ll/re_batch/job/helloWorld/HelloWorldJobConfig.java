2709ff4a7f03bec096170745e8a3fef453ec0cf6
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-12-19 01:27:29 +0900

    Feat : Step 2개 실행

--- src/main/java/com/ll/re_batch/job/helloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/ll/re_batch/job/helloWorld/HelloWorldJobConfig.java
@@ -23,28 +23,47 @@ public class HelloWorldJobConfig {
 
     // 하나의 Job 생성 -> 1job에는 다수의 step
     @Bean // 공유자원 -> 해당 객체를 어디서든지 사용할 수 있음
-    @JobScope // Bean, Session 등 각 영역마다 객체 생성
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
                 // Job이 성공하면 다시 실행되지 않는다. 하지만, increment로 다시 실행시킬 수 있다.
                 .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
                 .start(helloWorldStep1())
+                .next(helloWorldStep2())
                 .build();
     }
 
     // Job에 들어갈 step 생성
     @Bean
-    @StepScope
+    @JobScope // Bean, Session 등 각 영역마다 객체 생성
     public Step helloWorldStep1() {
         return stepBuilderFactory.get("helloWorldStep1")
-                .tasklet(helloWorldTasklet())
+                .tasklet(helloWorldStep1Tasklet())
                 .build();
     }
 
     @Bean
-    public Tasklet helloWorldTasklet() {
+    @StepScope
+    public Tasklet helloWorldStep1Tasklet() {
         return (contribution, chunkContext) -> {
-            System.out.println("헬로월드!");
+            System.out.println("tasklet1");
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    @JobScope
+    public Step helloWorldStep2() {
+        return stepBuilderFactory.get("helloWorldStep2")
+                .tasklet(helloWorldStep2Tasklet())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet helloWorldStep2Tasklet() {
+        return (contribution, chunkContext) -> {
+            System.out.println("tasklet2!");
 
             return RepeatStatus.FINISHED;
         };
